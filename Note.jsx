79145dbabc51c578eead6f0c14d504aed293587d
ajayydubey <78202775+ajayydubey@users.noreply.github.com>
2021-06-05 14:02:57 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>
+        WebDevelopment with ReactJs
+      </h1>
+      <p>This was an amazing bootcamp</p>
+
+    </div>
+
+  );
+}
+
+export default Note;
